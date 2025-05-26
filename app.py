fac67f6d2f5a3a9a9e17e0276c88720556749802
Anirudhharanagera <105443388+Anirudhharanagera@users.noreply.github.com>
2025-05-26 18:18:21 +0530

    Update app.py

--- app.py
+++ app.py
@@ -41,7 +41,7 @@ def load_models():
     )
 
 
-qa_model, qa_tokenizer, summarization_model, summarization_tokenizer, ner_pipeline, sentiment_pipeline = load_models()
+
 
 # ---- Load Cleaned Datasets ---- #
 @st.cache_data
